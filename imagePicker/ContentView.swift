4541912072feaf9781b0d46f7dfc2c152a7e84c5
Onore849 <rion3376@gmail.com>
2020-09-01 17:55:40 +0900

    first commit

--- imagePicker/ContentView.swift
+++ imagePicker/ContentView.swift
@@ -9,13 +9,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var imgData = Data(capacity: 0)
+    @State var shown = false
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack {
+            
+            Image(uiImage: UIImage(data: imgData)!).resizable().frame(height: 300).padding().cornerRadius(20)
+            
+            Button(action: {
+                
+                self.shown.toggle()
+                
+            }) {
+                
+                Text("Select Image")
+                
+            }.sheet(isPresented: $shown, onDismiss: {
+                
+                self.shown.toggle()
+                
+            }) {
+                
+                picker()
+                
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct picker: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<picker>) -> UIImagePickerController {
+        
+        let controller = UIImagePickerController()
+        controller.sourceType = .photoLibrary
+        
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<picker>) {
+        
+        
     }
 }
